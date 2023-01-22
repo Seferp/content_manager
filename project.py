dd46a06a33cdbc0e1a80dbe249c812acaa6d4571
P.Seferyniak <p.seferyniak@gmail.com>
2023-01-22 22:39:26 +0100

    Update gitignore

--- project.py
+++ project.py
@@ -1,16 +1,18 @@
 from os import listdir, rename, mkdir, path, remove
 
-class Content_Menager():
+
+
+class Content_Manager():
     def __init__(self, path, file_name, new_file_name, file_type):
         self.path = path
         self.file_name = file_name
         self.new_file_name = new_file_name
         self.file_type = file_type
-
+        self.folder_name = str(input("Enter the folder name: "))
 
     def rename_auto(self):
         files = listdir(self.path)
-        counter = int(input("Enter the starting numbering value: "))   # User can choose from which number wants start numbering
+        counter = int(input("Enter the starting numbering value: "))          # User can choose from which number wants start numbering
 
         for file in files:
             if file.endswith(self.file_type):
@@ -40,26 +42,25 @@ class Content_Menager():
 
 
     def make_direction(self):
-        qt_folders = int(input("How many folders you need: "))
+        quantity_folders = int(input("How many folders you need: "))
+        result = "The folder has been created"
             # raise
-        if qt_folders == 1:
-            folder_name = str(input("Enter the folder name: "))
-            path_folder = path.join(self.path, folder_name)
+        if quantity_folders == 1:
+            path_folder = path.join(self.path, self.folder_name)
             mkdir(path_folder)
-            print("The folder has been created")
+            print(result)
         else:
-            folder_name = str(input("Enter the folder name: "))
-            for number in range(1, qt_folders+1):
-                path_folder = path.join(self.path, f'{folder_name} ({str(number)})')
+            for number in range(1, quantity_folders+1):
+                path_folder = path.join(self.path, f'{self.folder_name} ({str(number)})')
                 mkdir(path_folder)
-            print("The folder's has been created")
+            print(result)
     def files_move(self):
         pass
 
     def delete_file(self):
         file_to_remove = str(input("Enter the name of file which would you delete: "))
-        remove_path = path.join(self.path)
-        remove()
+        remove_path = path.join(self.path,file_to_remove)
+        remove(remove_path)
 
     def searching_file(self):
         pass
---
+++ rename_app.py
@@ -0,0 +1,96 @@
+from os import listdir, rename, mkdir, path, remove
+import re
+
+
+# Automatyczne doliczanie nr pliku
+
+"""
+def rename(path, new_name, format):
+    files = os.listdir(path)
+    counter = 1
+    for file in files:
+        if file.endswith(format):
+            # file_type = file.split('.')[-1]
+            nname = new_name + ((len(str(len(files))) - len(str(counter))) * '0') + str(counter) + '.' + format
+            os.rename(f'{path}/{file}', f'{path}/{nname}')
+            counter += 1
+"""
+
+# files_dir = 'D:\_Python\Roboczy\\files_rename\\files_rename_first_idea'
+# files_dir = 'D:\_Python\Roboczy\\files_rename\\files_rename_second_idea'
+
+# files_dir = input("Enter the path: ")
+# print(type(files_dir))
+# frm = 'txt'
+# rename(files_dir, 'B', frm)
+
+
+# Ręczne podawanie nazwy
+"""
+def rename(path):
+    files = os.listdir(path)
+    for file in files:
+        file_type = file.split('.')[-1]
+        new_name = str(input())
+        os.rename(path + '/' + file, path + '/' + new_name + '.' + file_type)
+        print(f'Zmiana nazwy {file} na {new_name}.{file_type}')
+
+files_dir = 'D:\_Python\Roboczy\\files_rename\\files rename second idea\\1 To .docx'
+rename(files_dir)
+"""
+
+# Regex
+
+"""
+def rename(path, new_name, format):
+    files = os.listdir(path)
+    counter = 1
+    for file in files:
+        if re.match(format, file):
+            file_type = file.split('.')[-1]
+            nname = new_name + ((len(str(len(files))) - len(str(counter))) * '0') + str(counter)+ '.' + file_type
+            os.rename(f'{path}/{file}', f'{path}/{nname}')
+            counter += 1
+
+
+files_dir = 'D:\_Python\Roboczy\\files_rename\\files rename firsr idea'
+file_dir = os.path.abspath('Test0027')
+frm = '.*.txt'
+rename(files_dir, 'notatnik', frm)
+"""
+
+
+def make_direction(path):
+    qt_folders = int(input("How many folders you need: "))
+    # raise
+    if qt_folders == 1:
+        folder_name = str(input("Enter the folder name: "))
+        path_folder = path.join(path, folder_name)
+        mkdir(path_folder)
+    else:
+        # counter = 1
+        folder_name = str(input("Enter the folder name: "))
+        for number in range(1, qt_folders+1):
+            path_folder = path.join(path, f'{folder_name} ({str(number)})')
+            mkdir(path_folder)
+            # counter += 1
+
+x = "D:\_Python\Roboczy\\files_rename"
+y = "Test3"
+
+# make_direction(x)
+
+
+def delete_file(pathe):
+    file_to_remove = str(input("Enter the name of file which would you delete: "))
+    # if path.exists("demofile.txt"):
+    #     remove("demofile.txt")
+    # else:
+    #     print("The file does not exist")
+    frm = str(input("Enter the file type: "))
+    remove_path = path.join(pathe, file_to_remove+frm)
+    print(remove_path)
+    remove(remove_path)
+"""A0105"""
+x = "D:\_Python\Roboczy\\files_rename"
+delete_file(x)
