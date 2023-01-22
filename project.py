3d946ae21e2cea14af95155bcb8e176513bf7132
P.Seferyniak <p.seferyniak@gmail.com>
2023-01-22 20:20:26 +0100

    Create rename-manual fuction

--- project.py
+++ project.py
@@ -8,11 +8,21 @@ class Content_Menager():
         self.file_type = file_type
 
 
-    def rename_many_files(self):
+    def rename_auto(self):
         files = listdir(self.path)
-        counter = int(input())   # User can choose from which number wants start numbering
+        counter = int(input("Enter the starting numbering value."))   # User can choose from which number wants start numbering
         for file in files:
             if file.endswith(self.file_type):
-                new_name = self.new_file_name + counter + self. file_type
+                new_name = self.new_file_name + counter + self.file_type
                 rename(f'{self.path}/{file}', f'{self.path}/{new_name}')
                 counter += 1
+        print("Successful change of name")
+
+    def rename_manual(self):
+        files = listdir(self.path)
+        for file in files:
+            if file.endswith(self.file_type):
+                new_name = str(input("Enter the new name")) + self.file_type   # User chooses a filename
+                rename(f'{self.path}/{file}',f'{self.path}/{new_name}')
+        print("Successful change of name")
+
