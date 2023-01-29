821c8e2e4c6a86bb0aed146fe8ed674b7b2e391c
P.Seferyniak <p.seferyniak@gmail.com>
2023-01-29 23:57:38 +0100

    Working with test (on going)

--- test_project.py
+++ test_project.py
@@ -1,4 +1,4 @@
-from project import ContentManager
+from project import ContentManager, RenameAuto, RenameManual, NewDirection, Move, Remove, Search, Create
 import unittest
 import os
 import shutil
@@ -6,18 +6,26 @@ import shutil
 
 class TestRenameAuto(unittest.TestCase):
     def setUp(self):
-        self.path_dir = os.path.join(os.getcwd(), 'Test')
-        os.makedirs(self.path_dir, exist_ok=True)
-        for n in range(5):
-            open(f'{self.path_dir}/test_name{n}.txt', 'x')
+        self.path_dir = os.getcwd()+'\Test'
+        os.mkdir(self.path_dir)
+        open(os.path.join(self.path_dir,'test0.txt'), 'x').close()
+        open(os.path.join(self.path_dir,'test1.txt'), 'x').close()
+        open(os.path.join(self.path_dir,'test2.txt'), 'x').close()
+        open(os.path.join(self.path_dir,'test3.txt'), 'x').close()
+        open(os.path.join(self.path_dir,'test4.txt'), 'x').close()
+
     def test_rename_auto(self):
-        test = ContentManager(os.path.join(os.getcwd(), 'Test'), "", "", "", 'test_name', "", 0)
+        test = RenameAuto((os.getcwd()+'\Test'), 'txt', 'test_name')
         test.rename_auto()
-        files = os.listdir(self.path_dir)
-        for file in files:
-            self.assertTrue(os.path.exists(os.path.join(f'{self.path_dir,file}.txt')))
-    def tearDown(self):
-        shutil.rmtree(self.path_dir)
+        assert os.path.exists(os.path.join(self.path_dir,'test0.txt'))
+        assert os.path.exists(os.path.join(self.path_dir,'test1.txt'))
+        assert os.path.exists(os.path.join(self.path_dir,'test2.txt'))
+        assert os.path.exists(os.path.join(self.path_dir,'test3.txt'))
+        assert os.path.exists(os.path.join(self.path_dir,'test4.txt'))
+
+
+    # def tearDown(self):
+    #     shutil.rmtree(self.path_dir)
 
 
 # if __name__ == '__main__':
